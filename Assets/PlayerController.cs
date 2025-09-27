3a831adfe5fec45254eab7b5f6ca143341b032ab
Stas Gannutin <saucefishman@wireless-10-148-254-222.public.utexas.edu>
2025-09-26 20:31:09 -0500

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // new input system
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    private PlayerControls controls;
+    private Vector2 moveInput;
+    private bool jumpPressed;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Player.Jump.performed += ctx => jumpPressed = true;
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        if (jumpPressed && Mathf.Abs(rb.velocity.y) < 0.01f) // simple grounded check
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        jumpPressed = false;
+    }
+}
