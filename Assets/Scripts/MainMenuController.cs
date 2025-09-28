aea49d050e0b459151e855ad3dce49729cd175c5
saucefishman <stas.gannutin@gmail.com>
2025-09-28 16:13:08 -0500

    UI

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,6 +3,10 @@ using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
+    public Texture2D startButtonNormalTexture;
+    public Texture2D startButtonPressedTexture;
+    public Texture2D quitButtonNormalTexture;
+    public Texture2D quitButtonPressedTexture;
     private static string START_BUTTON_NAME = "startButton";
     private static string QUIT_BUTTON_NAME = "quitButton";
     
@@ -16,7 +20,26 @@ public class MainMenuController : MonoBehaviour
         startButton.clicked += () => {
             UnityEngine.SceneManagement.SceneManager.LoadScene(GAME_SCENE_NAME);
         };
+        startButton.RegisterCallback<MouseUpEvent>(evt =>
+        {
+            startButton.style.backgroundImage = startButtonNormalTexture;
+        });
+        startButton.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            startButton.style.backgroundImage = startButtonPressedTexture;
+        }, TrickleDown.TrickleDown);
+        
         var quitButton = root.Q<Button>(QUIT_BUTTON_NAME);
+        
+        quitButton.RegisterCallback<MouseUpEvent>(evt =>
+        {
+            quitButton.style.backgroundImage = quitButtonNormalTexture;
+        });
+        quitButton.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            quitButton.style.backgroundImage = quitButtonPressedTexture;
+        }, TrickleDown.TrickleDown);
+        
         quitButton.clicked += () => {
             Application.Quit();
         };
